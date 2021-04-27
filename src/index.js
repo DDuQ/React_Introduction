e3f2bbbd7cd5c73d8778b7e454038582bc96374d
DDuQ <61994091+DDuQ@users.noreply.github.com>
2021-04-27 14:28:21 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// var React = require("react");
+// var ReactDOM = require("react-dom"); (OLD WAY TO IMPORT)
+
+// ReactDOM.render(What to show, Where to show it);
+
+ReactDOM.render(
+<div>
+<h1>Hello World!</h1>
+<p>This is a paragraph.</p>
+</div>
+, document.getElementById("root"));
